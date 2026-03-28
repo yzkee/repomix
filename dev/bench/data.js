9de9e8a0ebe5a9b9b2a562f0d43345735d8db44b
github-action-benchmark <github@users.noreply.github.com>
2026-03-28 04:19:13 +0000

    add Repomix Performance (customSmallerIsBetter) benchmark result for 308c11b4c8606531dabc86ab9f84d21bd7534669

---
+++ dev/bench/data.js
@@ -0,0 +1,53 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1774671553301,
+  "repoUrl": "https://github.com/yzkee/repomix",
+  "entries": {
+    "Repomix Performance": [
+      {
+        "commit": {
+          "author": {
+            "email": "koukun0120@gmail.com",
+            "name": "Kazuki Yamada",
+            "username": "yamadashy"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "308c11b4c8606531dabc86ab9f84d21bd7534669",
+          "message": "Merge pull request #1323 from yamadashy/renovate/browser-non-major-dependencies\n\nchore(deps): update browser non-major dependencies",
+          "timestamp": "2026-03-28T11:24:57+09:00",
+          "tree_id": "3ebef7be38852e9a980619d94f4d76d812cba651",
+          "url": "https://github.com/yzkee/repomix/commit/308c11b4c8606531dabc86ab9f84d21bd7534669"
+        },
+        "date": 1774671552875,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Repomix Pack (macOS)",
+            "value": 2526,
+            "range": "±276",
+            "unit": "ms",
+            "extra": "Median of 20 runs\nQ1: 2427ms, Q3: 2703ms\nAll times: 2324, 2352, 2359, 2374, 2387, 2427, 2427, 2439, 2443, 2468, 2526, 2530, 2530, 2578, 2616, 2703, 2703, 2711, 2806, 2881ms"
+          },
+          {
+            "name": "Repomix Pack (Linux)",
+            "value": 2971,
+            "range": "±13",
+            "unit": "ms",
+            "extra": "Median of 10 runs\nQ1: 2962ms, Q3: 2975ms\nAll times: 2947, 2954, 2962, 2966, 2970, 2971, 2972, 2975, 3010, 3045ms"
+          },
+          {
+            "name": "Repomix Pack (Windows)",
+            "value": 4714,
+            "range": "±966",
+            "unit": "ms",
+            "extra": "Median of 10 runs\nQ1: 3963ms, Q3: 4929ms\nAll times: 3822, 3825, 3963, 4682, 4706, 4714, 4737, 4929, 4986, 18046ms"
+          }
+        ]
+      }
+    ]
+  }
+}
